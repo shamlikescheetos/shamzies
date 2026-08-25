2a3f9ba7b98d1acefa562297c46d2b8cf1d7e6e0
shamlikescheetos <stuf796854@moe.sch.ae>
2026-08-26 03:31:30 +0400

    Implement basic proxy server using node-unblocker

---
+++ server.js
@@ -0,0 +1,17 @@
+const Unblocker = require('node-unblocker');
+const http = require('http');
+
+const unblocker = new Unblocker({ prefix: '/proxy/' });
+
+http.createServer((req, res) => {
+    unblocker(req, res, (err) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end(err.stack || err.toString());
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Not Found');
+        }
+    });
+}).listen(process.env.PORT || 3000);
+console.log('Proxy running!');

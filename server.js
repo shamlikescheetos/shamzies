ff879ce7f58754e529a272e983494bade750fd72
shamlikescheetos <stuf796854@moe.sch.ae>
2026-08-27 16:58:43 +0400

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ const app = express();
 app.use(express.static(__dirname));
 app.use('/uv/', express.static(uvPath));
 
+// Fallback to route requests properly into Ultraviolet
+app.use((req, res, next) => {
+    if (req.url.startsWith('/uv/service/')) {
+        res.sendFile(path.join(uvPath, 'uv.config.js'));
+    } else {
+        next();
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 const server = http.createServer(app);
 server.listen(process.env.PORT || 3000, () => {
     console.log('Server active.');
